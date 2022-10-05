da5ff38ec7d1e3d921dd695787037a10e6faeb13
Dylan YU <dylan@Dylans-MacBook-Air.local>
2022-10-04 22:34:18 -0400

    solved 226

---
+++ src/_226_InvertBinaryTree/TreeNode.java
@@ -0,0 +1,21 @@
+package _226_InvertBinaryTree;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+
+
+}
