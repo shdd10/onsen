2f853bc8d349b9ea60b9d20a7b8a483fde170d02
Dylan YU <dylan@Dylans-MacBook-Air.local>
2023-01-24 22:37:05 -0500

    solved 225

---
+++ src/_225_ImplementStackusingQueues/MyStack.java
@@ -0,0 +1,34 @@
+package _225_ImplementStackusingQueues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+
+  Queue<Integer> queue;
+
+  public MyStack() {
+    queue = new LinkedList<>();
+  }
+
+  public void push(int x) {
+    queue.offer(x);
+    int size = queue.size();
+    while (size-- > 1) {
+      queue.offer(queue.poll());
+    }
+  }
+
+  public int pop() {
+    return queue.poll();
+  }
+
+  public int top() {
+    return queue.peek();
+  }
+
+  public boolean empty() {
+    return queue.isEmpty();
+  }
+
+}
